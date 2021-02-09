975a27b312c821778c4c13ac79caf2ef75b4c309
Ruben Rudov <59509727+rubenrudov@users.noreply.github.com>
2021-02-09 10:19:29 +0200

    Update main.py

--- server/main.py
+++ server/main.py
@@ -9,40 +9,6 @@ import json
 from datetime import datetime
 from pymongo import MongoClient
 
-"""
-Documentation for all the functions in this file: 
--------------
-
-1) Functions in this file that operates on the database:
-|
-|--- Users collection:
-|    add_new_user(username, email, password) -> inserts the username into the mongodb database. returns True 
-     if successful else False.
-|    get_account_password(username) -> returns the password for an account.
-|    is_exist_element(value, key) -> returns True if the user/email is already in the users collection else returns False
-|
-|--- Posts collection: 
-|    add_new_post(publisher, title, content, publishing_date) -> inserts the post into the mongodb database 
-|    get_all_posts() -> returns a json object with all the posts found in the posts collections
-|    exist_title(title) - > returns True if the post title is already exist else False
-
-2) Other functions:
-|--- main() -> calls all the functions
-|--- connection_handling() -> adds new connection and handles it
-
-About the database:
-    I've used mongodb as my database for this project because I like working with NoSQL databases.
-    This database is a json/document database that stored in cluster.
-    The collections in the database are: users and posts. 
-    To work with mongodb in python I've used the pymongo module (open source module that has to be installed with the pip)
-
-Overview:
-    Functions were written in name_of_function format.
-    For string formatting I've used the f"Variable value: {variable}" format.
-    The constant are used for database operations and communication.
-    The protocol of the communication is TCP.
-"""
-
 # Server & communication constants setting
 SERVER_SOCKET_AUTH = ("0.0.0.0", 8080)
 DATABASE_CLUSTER = MongoClient(
@@ -54,6 +20,12 @@ POSTS_COLLECTION = DATABASE["posts"]
 
 # User collection related functions
 def add_new_user(username: str, email: str, password: str):
+    """
+    :param username: string that contains the username
+    :param email: string that contains the email
+    :param password: string that contains the password
+    :return: bool that represents the result of the insert to the database (successful -> True, unsuccessful -> False)
+    """
     print(f"User data: {username}, {email}, {password}")
     post = {"username": username, "email": email, "password": password}
 
@@ -66,6 +38,10 @@ def add_new_user(username: str, email: str, password: str):
 
 
 def get_account_password(username: str):
+    """
+    :param username: string that represents the username
+    :return: the password of the specific username in the database
+    """
     search_results = USERS_COLLECTION.find({"username": username})
     for search_result in search_results:
         return search_result["password"]
@@ -74,6 +50,11 @@ def get_account_password(username: str):
 
 
 def is_exist_element(value: str, key: str):
+    """
+    :param value: string that contains a value
+    :param key: string that contains the searching key {key: value}
+    :return: if exist -> True else -> False
+    """
     print(f"value: {value} and {key} as key")
 
     if USERS_COLLECTION.find({key: value}) is {}:
@@ -85,34 +66,54 @@ def is_exist_element(value: str, key: str):
 
 # Posts collection related
 def add_new_post(publisher: str, title: str, content: str, publishing_date: str):
-    print(f"Post data: {publisher}, {title}, {content} {publishing_date}")
-    post = {"_id": POSTS_COLLECTION.count() + 1, "publisher": publisher, "title": title, "content": content, "publishing_date": publishing_date}
+    """
+    :param publisher: string that contains the username of the post publisher
+    :param title: string that contains the post title
+    :param content: string that contains the post content
+    :param publishing_date: string that contains the post publishing date and time
+    :return:
+    """
+    __id = POSTS_COLLECTION.count() + 1     # The id of the next
+
+    post = {"_id": __id, "publisher": publisher, "title": title, "content": content,
+            "publishing_date": publishing_date}
 
     if not exist_title(title):
         POSTS_COLLECTION.insert_one(post)
+
+        # For debugging, check if the value was inserted
+        mongo_cursor_result = POSTS_COLLECTION.find({"__id": __id})
+        print(json.dumps(mongo_cursor_result))
         return True
 
     print("Debug: post title is already exist")
     return False
 
 
-def get_all_posts(client_sock: socket.socket):
-    all_posts = POSTS_COLLECTION.find({})  # gets all the posts
+def get_all_posts():
+    """
+    :return: a json of all the posts found in the POST_COLLECTION of the database
+    """
+    all_data = {}  # Data to send
+    __key = 0       # "Private key"
+    mongo_cursor_result = POSTS_COLLECTION.find({})    # Running a searching query for mongo
 
-    full_data = []
+    value = mongo_cursor_result[__key]
+    print(value)
+    print(type(value))  # still a dict
+    print(type(json.dumps(value)))      # str
 
-    for post in all_posts:
-        full_data.append(post)
+    while value is not None:
+        all_data[__key] = value
 
-    for data_piece in full_data:
-        print(f"Sending {data_piece} to the client")
-        client_sock.send(json.dumps(data_piece).encode())
-        data_received = client_sock.recv(1024).decode()
-        print(f"Received: {data_received}")
+    return all_data
 
 
 def exist_title(title: str):
-
+    """
+    :param title: string that contains the post title
+    :return: if exist -> True, else -> False
+    """
     if POSTS_COLLECTION.find({"title": title}) is {}:
         print("Exist")
         return True
@@ -122,6 +123,11 @@ def exist_title(title: str):
 
 # Connection & communication handling
 def request_handling(client_socket: socket.socket, data_struct):
+    """
+    :param client_socket: a connection socket between the client to the server
+    :param data_struct: a json that contains the request and other params that matches the request
+    :return: None
+    """
     if data_struct["request"] == "add_user":
         response = add_new_user(
             data_struct["username"],
@@ -158,20 +164,23 @@ def request_handling(client_socket: socket.socket, data_struct):
         client_socket.send(json.dumps(data).encode())
 
     elif data_struct["request"] == "get_all_posts":
-        get_all_posts(client_socket)
-        data = {"response": "finished"}
-        client_socket.send(json.dumps(data).encode())
+        data = get_all_posts()
+        for data_piece in data.items():
+            sending = {"response": data_piece[1]}
+            client_socket.send(json.dumps(sending).encode())
 
-    # elif data_struct["request"] == "get_specific_post":
     #   TODO: Add mongodb function for this operation
-    #
-    #
 
     else:
-        client_socket.send("Unknown command".encode())      # TODO: Send an "unknown request" message to the user
+        client_socket.send(json.dumps({"response": "Unknown command, Debug mode..."}))
 
 
 def new_connection(client_socket: socket.socket, ip_address: str):
+    """
+    :param client_socket: a connection socket between the client to the server
+    :param ip_address: a string that represents an ip address
+    :return: a dictionary
+    """
     print(f"Debug: {ip_address} has connected to the server via {client_socket}")
     data = client_socket.recv(1024)
     decoded_data = data.decode()
@@ -182,6 +191,10 @@ def new_connection(client_socket: socket.socket, ip_address: str):
 
 
 def main():
+    """
+    The main function of the server, which controls the connections receiving with threading  
+    :return: None
+    """
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(SERVER_SOCKET_AUTH)
     server_socket.listen()  # Listens for unlimited amount of clients
